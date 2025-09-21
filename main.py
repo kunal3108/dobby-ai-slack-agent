0d61d2cd1800b6cf9025af282e982c3fc407de0b
kunal3108 <132456942+kunal3108@users.noreply.github.com>
2025-09-22 02:23:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from slack_listener.listener import SlackListener
-
 if __name__ == "__main__":
+    print("🤖 Slack bot is starting...")
     listener = SlackListener()
     listener.start_listening()
